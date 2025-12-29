ec8ddabf4d49fdd68a4a946b49d6b666c5f074c5
TodoSota <137256865+TodoSota@users.noreply.github.com>
2025-12-29 19:04:15 +0900

    [add]mpmObjectの追加。
    MPMにおけるデータ群のパッケージであるmpmObjectの追加。

---
+++ mpmObject.cpp
@@ -0,0 +1,57 @@
+/// 図形関連の処理
+#include "mpmObject.h"
+
+/// コンストラクタ
+/// @param[in] count 頂点数
+/// @param[in] gridSize グリッド(3Dテクスチャ)の解像度
+mpmObject::mpmObject(GLsizei count, int gridSize) :
+	// 頂点配列オブジェクトを作成して vao を作成する
+	vao{ []() {GLuint vao; glGenVertexArrays(1, &vao); return vao; }() },
+	// 頂点バッファオブジェクトを作成して vbo を作成する
+	vbo{ []() {GLuint vbo; glGenBuffers(1, &vbo); return vbo; }() },
+	// 頂点の数を保存
+	count{ count },
+	// テクスチャを作成
+	gridTex{ []() {GLuint t; glGenTextures(1, &t); return t; }() },
+	// グリッドサイズを保存
+	gridSize{ gridSize }
+{
+	// バッファの設定
+	glBindVertexArray(vao);// vaoを結合
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);// vboを結合しvaoに組み込む
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * count, nullptr, GL_DYNAMIC_DRAW);// vboのメモリを確保し頂点位置データを転送
+
+	/// [in]変数 0 番に position
+	/// [in]変数 1 番に state(色分けで状態を表示)
+	 
+	// 結合されているvboの position を 0 番として設定
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Particle), (void*)offsetof(Particle, position)) ;
+	glEnableVertexAttribArray(0); // 0 番のvboを有効に
+
+	// 結合されているvboの state を 1 番として設定
+	glVertexAttribIPointer(1, 1, GL_INT, sizeof(Particle), (void*)offsetof(Particle, state)); // int なので 1 成分
+	glEnableVertexAttribArray(1); // 1 番のvboを有効に
+
+	// 3Dテクスチャ(MPMグリッド)の設定
+	// RGBA32F を使用: RGB =運動量(mvx, mvy, mvz), A=質量(m)
+	glBindTexture(GL_TEXTURE_3D, gridTex);
+	glTexImage3D(GL_TEXTURE_3D, 0, GL_RGBA32F, gridSize, gridSize, gridSize, 0, GL_RGBA, GL_FLOAT, nullptr);
+	
+	// 正確なノードへの Read/Write のため NEAREST で
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	// 処理終了の後始末
+	glBindTexture(GL_TEXTURE_3D, 0);	// テクスチャの選択を解除
+	glBindVertexArray(0);				// vaoの結合を解除する
+	glBindBuffer(GL_ARRAY_BUFFER, 0);	// vboの結合を解除する
+}
+
+mpmObject::~mpmObject() {
+	glDeleteVertexArrays(1, &vao);	// vaoを削除
+	glDeleteBuffers(1, &vbo);		// vboを削除
+	glDeleteTextures(1, &gridTex);	//3Dテクスチャを削除
+}
---
+++ mpmObject.h
@@ -0,0 +1,51 @@
+#pragma once
+
+// バッファオブジェクト関連の宣言はgl.h は含まれていないので glew.h を使う
+#include <GL/glew.h>
+
+// GLM 関連
+#if !defined(_USE_MATH_DEFINES)
+#	define _USE_MATH_DEFINES
+#endif
+#if !defined(GLM_FORCE_RADIANS)
+#	define GLM_FORCE_RADIANS
+#endif
+#include <GLM/glm.hpp>
+
+/// 粒子の物理量
+struct Particle {
+
+	alignas(16) glm::vec4 position;		// 位置
+	alignas(16) glm::vec3 velocity;		// 速度
+	alignas(16) glm::mat3 affineC;		//アフィン速度行列
+	alignas(16) glm::mat3 deformation;	// 変形勾配
+
+	// 砂の塑性変形パラメーター
+	alignas(4) float alpha;		//降伏面の大きさ
+	alignas(4) float q;			//硬化状態
+	alignas(4) float vc;		//変化の際の体積変化
+	alignas(4) int state;		//状態(変化)
+
+};
+
+/// 頂点配列オブジェクト
+struct mpmObject {
+
+	const GLuint vao;		// vao:描画
+	const GLuint vbo;		// vbo:粒子のデータ本体
+	const GLsizei count;	// 頂点数
+
+	// MPMグリッドのフィールド
+	const GLuint gridTex;	// 3DテクスチャID
+	const int gridSize;		// グリッドのサイズ(テクスチャの解像度)
+
+	/// コンストラクタ
+	/// @param[in] count 頂点の数
+	/// @param[in] gridSize グリッド(3Dテクスチャ)の解像度
+	mpmObject(GLsizei count, int gridSize);
+
+	mpmObject(const mpmObject& mpmObject) = delete;					// コピーコンストラクタを禁止
+	virtual ~mpmObject();											// デコンストラクタ
+	mpmObject& operator = (const mpmObject& mpmObject) = delete;	// 代入演算子は使用しない
+	mpmObject& operator=(mpmObject&& mpmObject) = default;			// ムーブ代入演算子はデフォルトを使用
+};
