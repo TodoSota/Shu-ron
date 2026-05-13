05928b5c9b49b58bf5883a0da55cda0ebfd55a39
TodoSota <137256865+TodoSota@users.noreply.github.com>
2026-05-13 16:20:18 +0900

    [chore]READMEの更新
    READMEの更新
    メディアの追加

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@
 #include <GLM/gtc/matrix_transform.hpp>
 
 // 粒子数
-const auto PARTICLE_COUNT{ 10000 }; // ノートPCでやるには10000重いので
+const auto PARTICLE_COUNT{ 30000 }; // ノートPCでやるには10000重いので
 const float worldScale = 0.67f;
 
 /// 点群データ作成
